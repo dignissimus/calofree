4d3987655239d0dd4b196f4a3567c4d99bafe210
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-04-09 21:24:34 +0100

    Initial commit

---
+++ src/calofree.ts
@@ -0,0 +1,24 @@
+const NUMERICAL_INFORMATION_REGEX : RegExp = /\d(,?\d)*\s*(kcal|cal|calories?|kj)/ig
+
+function hideCalorieInformation(element: ChildNode) {
+    for (let node of element.childNodes) {
+        switch (node.nodeType) {
+            case Node.ELEMENT_NODE:
+                hideCalorieInformation(node);
+                break;
+            case Node.TEXT_NODE:
+                node.textContent = node.textContent?.replace(NUMERICAL_INFORMATION_REGEX, "[hidden]") || null;
+                break;
+            case Node.DOCUMENT_NODE:
+                hideCalorieInformation(node);
+        }
+    }
+}
+
+function calofree(){
+    if (true) {
+        hideCalorieInformation(document.body);
+    }
+}
+
+calofree()
