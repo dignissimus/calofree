66aa4f304e4dbfaf2c6ceeeb35ccadcbfbba30d5
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-04-10 14:29:42 +0100

    Allow revealing calorie information

--- src/calofree.ts
+++ src/calofree.ts
@@ -1,13 +1,62 @@
 const NUMERICAL_INFORMATION_REGEX: RegExp = /\d([,.]?\d)*\s*(k?cal(ories?)?|kj)/ig
 
+interface TextBreak {
+    node: Node;
+    start: number;
+    end: number;
+}
+
 function hideCalorieInformation(element: ChildNode) {
-    for (let node of element.childNodes) {
+    for (let node of Array.from(element.childNodes)) {
         switch (node.nodeType) {
             case Node.ELEMENT_NODE:
                 hideCalorieInformation(node);
                 break;
             case Node.TEXT_NODE:
-                node.textContent = node.textContent?.replace(NUMERICAL_INFORMATION_REGEX, "[hidden]") || null;
+                let originalTextContent = node.textContent!;
+                const breaks: TextBreak[] = [];
+                let previousOffset = 0;
+                let previousNode = node;
+                originalTextContent.replace(
+                    NUMERICAL_INFORMATION_REGEX,
+                    function(match, _p1, _p2, _p3, offset, string){
+                        breaks.push(
+                            {
+                                node: previousNode,
+                                start: previousOffset,
+                                end: offset
+                            }
+                        );
+
+                        previousOffset = offset + match.length;
+
+                        const newNode = document.createElement("a");
+                        newNode.addEventListener(
+                            "mouseover",
+                            function(event){
+                                (<HTMLElement> event.target).textContent = match;
+                            }
+                        );
+                        newNode.addEventListener(
+                            "mouseout",
+                            function(event){
+                                (<HTMLElement> event.target).textContent = "[hidden]";
+                            }
+                        );
+                        newNode.textContent = "[hidden]";
+                        previousNode.after(newNode);
+
+                        const followingNode = document.createTextNode(
+                            originalTextContent.substring(previousOffset)
+                        );
+                        newNode.after(followingNode);
+
+                        previousNode = followingNode;
+                        return "";
+                    }
+            ) || null;
+                for (let textBreak of breaks)
+                    textBreak.node.textContent = originalTextContent.substring(textBreak.start, textBreak.end);
                 break;
             case Node.DOCUMENT_NODE:
                 hideCalorieInformation(node);
