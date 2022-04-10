633553e254f3349fbabda3f2124286409f4f4cb8
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-04-10 19:57:59 +0100

    Add interface for option support

--- src/calofree.ts
+++ src/calofree.ts
@@ -1,10 +1,58 @@
 const NUMERICAL_INFORMATION_REGEX: RegExp = /\d([,.]?\d)*\s*(k?cal(ories?)?|kj)/ig
-const DELETE_ENTIRELY_REGEX: RegExp = /:?\s*\(\d([,.]?\d)*\s*(k?cal(ories?)?|kj)\)/ig
+const DELETE_ENTIRELY_REGEX: RegExp = /:?=?\s*\(?\d([,.]?\d)*\s*(k?cal(ories?)?|kj)\)?/ig
 
 interface TextBreak {
     node: Node;
     start: number;
     end: number;
+}
+
+enum RemovalOption {
+    Censor,
+    Cover,
+    Delete
+}
+
+function getCalofreeRegex(): RegExp {
+    switch(getCalofreeOption()) {
+        case RemovalOption.Cover:
+        case RemovalOption.Censor:
+            return NUMERICAL_INFORMATION_REGEX;
+        case RemovalOption.Delete:
+        default:
+            return DELETE_ENTIRELY_REGEX;
+    }
+}
+
+function getCalofreeOption(): RemovalOption {
+    return RemovalOption.Cover;
+}
+
+function createHiddenNode(match: string) {
+    switch(getCalofreeOption()){
+        case RemovalOption.Cover:
+            const hiddenNode = document.createElement("a");
+            hiddenNode.textContent = "[hidden]";
+            hiddenNode.addEventListener(
+                "mouseover",
+                function(event){
+                    (<HTMLElement> event.target).textContent = match;
+                }
+            );
+            hiddenNode.addEventListener(
+                "mouseout",
+                function(event){
+                    (<HTMLElement> event.target).textContent = "[hidden]";
+                }
+            );
+            return hiddenNode;
+        case RemovalOption.Censor:
+            return document.createTextNode("[hidden]");
+        case RemovalOption.Delete:
+            return document.createTextNode("");
+        default:
+            return document.createTextNode("");
+    }
 }
 
 function hideCalorieInformation(element: ChildNode) {
@@ -19,7 +67,7 @@ function hideCalorieInformation(element: ChildNode) {
                 let previousOffset = 0;
                 let previousNode = node;
                 originalTextContent.replace(
-                    NUMERICAL_INFORMATION_REGEX,
+                    getCalofreeRegex(),
                     function(match, _p1, _p2, _p3, offset, string){
                         breaks.push(
                             {
@@ -31,20 +79,8 @@ function hideCalorieInformation(element: ChildNode) {
 
                         previousOffset = offset + match.length;
 
-                        const newNode = document.createElement("a");
-                        newNode.addEventListener(
-                            "mouseover",
-                            function(event){
-                                (<HTMLElement> event.target).textContent = match;
-                            }
-                        );
-                        newNode.addEventListener(
-                            "mouseout",
-                            function(event){
-                                (<HTMLElement> event.target).textContent = "[hidden]";
-                            }
-                        );
-                        newNode.textContent = "[hidden]";
+                        const newNode = createHiddenNode(match);
+
                         previousNode.after(newNode);
 
                         const followingNode = document.createTextNode(
